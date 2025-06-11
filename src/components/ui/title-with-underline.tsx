5f838273126f453749a42535d464e1add77187bf
Gangezwer <gangezwer@gmail.com>
2025-06-12 00:01:07 +0530

    title with underline issue resolved.

--- src/components/ui/title-with-underline.tsx
+++ src/components/ui/title-with-underline.tsx
@@ -16,9 +16,9 @@ export const TitleWithUnderline: React.FC<TitleWithUnderlineProps> = ({
   return (
     <div className="relative">
       <svg
-        width="720"
+        width="574"
         height="46"
-        viewBox="0 0 720 46"
+        viewBox="0 0 574 46"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className="title max-w-[22rem]"
