0627337d1e873acd5228252b434eb9b97f141ffa
Gangezwer <gangezwer@gmail.com>
2025-06-15 15:55:34 +0530

    type issue fixed

--- src/components/podcast/PodcastSection.tsx
+++ src/components/podcast/PodcastSection.tsx
@@ -156,11 +156,7 @@ const PodcastSection = ({
               <div className="aspect-video rounded-md overflow-hidden mb-4">
                 <img
                   src={podcast.image}
-<<<<<<< HEAD
                   alt={typeof podcast.title === "string" ? podcast.title : ""}
-=======
-                  alt={podcast.title as string}
->>>>>>> a065f93ced5ea012e3fcff76758c546412f0a1c9
                   className="w-full h-full object-cover"
                 />
               </div>
