a3d794ec4d348a3ef631f3fca84b67eea452e786
Gangezwer <gangezwer@gmail.com>
2026-01-14 01:22:13 +0530

    all vulnerablities are fixed.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,40 +11,39 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-  {
-    files: ["**/*.{js,jsx,ts,tsx,mjs}"],
-    rules: {
-      "prettier/prettier": [
-        "error",
-        {
-          singleQuote: true,
-          trailingComma: "all",
-          tabWidth: 2,
-          semi: true,
-          printWidth: 100,
-          arrowParens: "always",
-          endOfLine: "auto",
-          jsxSingleQuote: true,
-          bracketSpacing: true,
-          jsxBracketSameLine: false,
-        },
-      ],
-      "no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          destructuredArrayIgnorePattern: "^_",
-        },
-      ],
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-    },
-    plugins: ["prettier"],
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.extends("prettier"), {
+  files: ["**/*.{js,jsx,ts,tsx,mjs}"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "all",
+        tabWidth: 2,
+        semi: true,
+        printWidth: 100,
+        arrowParens: "always",
+        endOfLine: "auto",
+        jsxSingleQuote: true,
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+      },
+    ],
+    "no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
   },
-];
+  plugins: ["prettier"],
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
