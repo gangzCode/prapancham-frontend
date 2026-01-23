70e2f2f4c7d6c199d43fe7335a6c19c4fe441643
Gangezwer <gangezwer@gmail.com>
2026-01-23 19:25:33 +0530

    AWS S3 bucket issue resolved.

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "prapanchamtest-asset.s3.eu-north-1.amazonaws.com",
+        hostname: "prapancham-prod-assets.s3.us-east-1.amazonaws.com",
         pathname: "/**",
       },
     ],
